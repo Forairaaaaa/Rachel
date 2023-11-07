ab13f5accd13fed84a75cd937262a51c356e39c5
Forairaaaaa <applesyqd@outlook.com>
2023-11-07 09:21:49 +0800

    pio init

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file main.cpp
+ * @author Forairaaaaa
+ * @brief 
+ * @version 0.1
+ * @date 2023-11-07
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#include <Arduino.h>
+
+
+void setup()
+{
+
+}
+
+
+void loop()
+{
+
+}
